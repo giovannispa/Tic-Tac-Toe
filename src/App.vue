0a38e0766cd617945203f5e9235d67ef180a30b3
Giovnni Sertorio <givanni.spadoni10@hotmail.com>
2023-01-17 22:55:15 -0300

    Refactoring and switching to Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
-<script setup>
-
-import TicTacToe from "./components/TicTacToe.vue";
-</script>
-
 <template>
   <div class="container mx-auto flex justify-center">
     <h1 class="text-7xl">Tic Tac Toe!!</h1>
   </div>
   <TicTacToe />
 </template>
+
+<script setup>
+
+import TicTacToe from "./components/TicTacToe.vue";
+</script>
 
 <style scoped>
 
--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -1,112 +1,88 @@
 <template>
   <div class="container mx-auto flex justify-center">
-    <h1 class="text-5xl">Next player: {{ this.currentPlayer }}</h1>
+    <h1 v-if="winner" class="text-5xl">Winner: {{ winner }}</h1>
+    <h1 v-else class="text-5xl">Player: {{ currentPlayer }}</h1>
   </div>
   <base-container v-for="(row, rowIndex) in board">
-    <box v-for="(cell, cellIndex) in row" v-on:click="handleMove(rowIndex, cellIndex)">
+    <box v-for="(cell, cellIndex) in row" @click="handleMove(rowIndex, cellIndex)">
       <Item>
         {{ cell }}
       </Item>
     </box>
   </base-container>
+
+  <div class="mx-auto flex justify-center">
+    <button class="text-5xl px-4 py-2 font-semibold bg-cyan-500 text-white rounded-full shadow-sm" v-if="this.winner" @click="resetBoard">Reset Game</button>
+  </div>
 </template>
 
 <script>
 import BaseContainer from "./BaseContainer.vue";
 import Box from "./Box.vue";
 import Item from "./Item.vue";
+import {ref, computed} from 'vue';
+
+const checkWinner = (board) => {
+  for(let i = 0; i < board.value.length; i++) {
+    //Checando se houve vencedor em uma linha.
+    if(board.value[i][0] === board.value[i][1] && board.value[i][1] === board.value[i][2]) {
+      return board.value[i][0];
+    }
+    //Checando se houve vencedor em uma coluna.
+    if(board.value[0][i] === board.value[1][i] && board.value[1][i] === board.value[2][i]) {
+      return board.value[0][i];
+    }
+    //Checando de houve vencedor em diagonal.
+    if(board.value[0][0] === board.value[1][1] && board.value[1][1] === board.value[2][2]) {
+      return board.value[0][0];
+    }
+    if(board.value[0][2] === board.value[1][1] && board.value[1][1] === board.value[2][0]) {
+      return board.value[0][2];
+    }
+  }
+  return null;
+};
 
 export default {
   name: "TicTacToe",
-  components: {Item, Box, BaseContainer},
-  data() {
-    return {
-      board: [
+  setup() {
+    const board = ref([
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ]);
+
+    const currentPlayer = ref("X");
+
+    const winner = computed(() => checkWinner(board));
+
+    const handleMove = (row, cell) => {
+
+      //Verificando se já existe um vencedor.
+      if(winner.value) {
+        return;
+      }
+
+      //Verificando se o campo clicado é válido.
+      if(board.value[row][cell] === null) {
+        //Atribuindo o simbolo clicado pelo player.
+        board.value[row][cell] = currentPlayer.value;
+
+        currentPlayer.value = currentPlayer.value === "X" ? "O" : "X";
+      }
+    };
+
+    const resetBoard = () => {
+      board.value = [
         [null, null, null],
         [null, null, null],
         [null, null, null]
-      ],
-      currentPlayer: "X",
+      ];
+      currentPlayer.value = "X";
     }
+
+    return { board, currentPlayer, winner, handleMove, resetBoard }
   },
-  methods: {
-      resetBoard() {
-        for(let i = 0; i < this.board.length; i++) {
-          for(let j = 0; j < this.board[i].length; j++) {
-            this.board[i][j] = null;
-          }
-        }
-      },
-      checkDraw(board) {
-        let endGame = true;
-        for(let i = 0; i < board.length; i++) {
-          for(let j = 0; j < board[i].length; j++) {
-            if(board[i][j] === null) {
-              endGame = false;
-            }
-          }
-        }
-
-        return endGame;
-      },
-      checkWinner(board) {
-        for(let i = 0; i < board.length; i++) {
-
-          //Checando se houve vencedor em uma linha.
-          if(board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
-            return board[i][0];
-          }
-
-          //Checando se houve vencedor em uma coluna.
-          if(board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
-            return board[0][i];
-          }
-
-          //Checando de houve vencedor em diagonal.
-          if(board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-            return board[0][0];
-          }
-
-          if(board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-            return board[0][2];
-          }
-
-        }
-
-        return null;
-      },
-      handleMove(row, cell) {
-        //Verificando se o campo clicado é válido.
-        if(this.board[row][cell] === null) {
-
-          //Atribuindo o simbolo clicado pelo player.
-          this.board[row][cell] = this.currentPlayer;
-        }
-
-        //Checa se já á um vencedor.
-        let winner = this.checkWinner(this.board);
-
-        if(winner !== null) {
-          //Avisando quem foi o vencedor.
-          alert("Player " + winner + " is the winner!");
-
-          //Zerando o tabuleiro.
-          this.resetBoard();
-        }
-
-        //Checando se o jogo já acabou
-        if(this.checkDraw(this.board)) {
-
-          //Se o jogo acabou sem um vencedor, avisa o empate.
-          alert("Draw!!!!!!!!!!");
-
-          //Zerando o tabuleiro.
-          this.resetBoard();
-        }
-
-        //Alterando para o próximo jogador.
-        this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
-      }
-  }
+  components: {BaseContainer, Box, Item}
 }
 </script>
